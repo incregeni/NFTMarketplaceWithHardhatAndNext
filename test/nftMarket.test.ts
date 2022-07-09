7c324060f37bb295d4677b1ea5ffb166e044b1ad
incregeni <incregeni@gmail.com>
2022-07-09 18:11:25 -0300

    create tests

---
+++ test/nftMarket.test.ts
@@ -0,0 +1,45 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { assert, expect } from "chai";
+import { Contract, ContractFactory } from "ethers";
+import { ethers, network } from "hardhat";
+import { NFT, NFTMarket } from "../typechain-types";
+
+describe("NFTMarket", function () {
+  let Market: ContractFactory;
+  let market: Contract;
+  let NFT: ContractFactory;
+  let nft: Contract;
+  let deployer: SignerWithAddress;
+  let buyer: SignerWithAddress;
+  let accounts: SignerWithAddress[];
+  let marketDeployedAddress: string;
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    buyer = accounts[1];
+    Market = await ethers.getContractFactory("NFTMarket");
+    market = await Market.deploy();
+    await market.deployed();
+    marketDeployedAddress = market.address;
+
+    NFT = await ethers.getContractFactory("NFT");
+    nft = await NFT.deploy(marketDeployedAddress);
+    await nft.deployed();
+  });
+
+  describe("constructor", function () {
+    it("intitiallizes the nft market correctly", async () => {
+      const owner = await market.getOwner();
+      assert.equal(owner, deployer.address);
+    });
+  });
+
+  describe("Listing Fee", function () {
+    it("listing fee should be 0.0025 ethers", async () => {
+      const listingFee = (await market.getListingFee()).toString();
+      const fee = ethers.utils.formatEther(listingFee);
+      assert.equal(fee, "0.0025");
+    });
+  });
+});
